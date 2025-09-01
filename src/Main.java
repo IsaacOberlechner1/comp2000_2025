9ecbf874d790ea26a26a55557e0334adfc96f8a9
Isaac Oberlechner <isaacdoberlechner@gmail.com>
2025-09-01 11:25:54 +1000

    New Changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("COMP2000 2025");
+        System.out.println("Red vs. Blue");
     }
 }
