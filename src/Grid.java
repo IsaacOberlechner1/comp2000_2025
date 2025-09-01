089fe2b2afb8cd44b86c1a44538b6b7df4775e71
Isaac Oberlechner <isaacdoberlechner@gmail.com>
2025-09-01 12:15:17 +1000

    Added Note

--- src/Grid.java
+++ src/Grid.java
@@ -15,7 +15,7 @@ public class Grid {
   public void paint(Graphics g, Point mousePos) {
     for(int i=0; i<cells.length; i++) {
       for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
+        cells[i][j].paint(g, mousePos); // 
       }
     }
   }
