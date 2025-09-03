6da57dd8319a4bd19ff9bd14199e7ab4644754f8
Isaac Oberlechner <isaacdoberlechner@gmail.com>
2025-09-03 16:04:48 +1000

    Completed Tasks 10 and 11

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Actor {
+  List<Polygon> polygons = new ArrayList<Polygon>();
   Color color;
   Cell loc;
 
   public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    for(int i = 0; i < polygons.size(); i++){
+      g.setColor(color);
+      g.fillPolygon(polygons.get(i));
+      g.setColor(Color.BLACK);
+      g.drawPolygon(polygons.get(i));
+    }
+
   }
 }
--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,23 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Stage {
   Grid grid;
-  Actor cat;
-  Actor dog;
-  Actor bird;
+  List<Actor> characters = new ArrayList<Actor>();
 
   public Stage() {
     grid = new Grid();
-    cat = new Cat(grid.cellAtColRow(0, 0));
-    dog = new Dog(grid.cellAtColRow(0, 15));
-    bird = new Bird(grid.cellAtColRow(12, 9));
+    characters.add(new Cat(grid.cellAtColRow(0, 0)));
+    characters.add(new Dog(grid.cellAtColRow(0, 15)));
+    characters.add(new Bird(grid.cellAtColRow(12, 9)));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+    for(int i = 0; i < characters.size(); i++){
+      characters.get(i).paint(g);
+    }
   }
 }
