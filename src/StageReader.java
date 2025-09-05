abf5d27ca00eec3e601d00497c3513527ea9c46b
Isaac Oberlechner <isaacdoberlechner@gmail.com>
2025-09-05 16:01:46 +1000

    Build: expand 'StageManager' class
    Added a for-loop to 'StageManager' cycling through the lines list. Allows access to each String in the list for further subdivision and exception handling.

--- src/StageReader.java
+++ src/StageReader.java
@@ -2,11 +2,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.io.IOException;
 
 public class StageReader {
   public static Stage readStage(String path) throws IOException {
     Stage stage = new Stage();
     List<String> lines = Files.readAllLines(Paths.get(path));
+    for (int i = 0; i < lines.size(); i++){
+      
+    }
     return new Stage();
   }
 }
